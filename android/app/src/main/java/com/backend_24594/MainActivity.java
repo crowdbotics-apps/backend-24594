32b3034a3eafe979c0595d298f4b843eb716da32
Crowdbotics <team@crowdbotics.com>
2021-02-18 10:23:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/backend_24594/MainActivity.java
@@ -0,0 +1,15 @@
+package com.backend_24594;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "backend_24594";
+    }
+}
